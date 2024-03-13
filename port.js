ccfd9b374de8627caa4bca014b6b8b5df81c5b3d
Abhinav.K <145023443+abhinavkothakota@users.noreply.github.com>
2024-03-13 14:44:42 +0530

    commit

---
+++ port.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".Text",{
+    strings:["Front-End Developer","Photographer","Editor","Competetive Gamer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+
+});
