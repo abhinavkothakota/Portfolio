404c83e33beb960e8b195da295211af4d87b2a33
Kothakota Abhinav <2003abhinavk@gmail.com>
2025-04-10 09:31:37 +0530

    links

--- REACT/Reactfolio/My-Portfolio-main/src/Components/ProjectBox.jsx
+++ REACT/Reactfolio/My-Portfolio-main/src/Components/ProjectBox.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaFigma } from "react-icons/fa";
+import { CgFileDocument } from "react-icons/cg";
 import PropTypes from 'prop-types';
 
 const ProjectBox = ({ projectPhoto, projectName }) => {
   const desc = {
     CarPointDesc:
       "Designed and developed a Vehicle Service Application using modern web technologies. The platform enables users to schedule and manage vehicle maintenance, while service providers can efficiently handle appointments and track inventory.",
-    CarPointGithub: "https://ak-carpoint.vercel.app",
+    CarPointWebsite: "https://ak-carpoint.vercel.app",
+    CarPointGithub:"https://github.com/abhinavkothakota/VehicleServiceManagement-CARPOINT",
     FoodAppDesc:
       "Designed a mobile food app in Figma, focusing on UI/UX to ensure a seamless experience for browsing menus, placing orders, and managing deliveries.",
     FoodAppFigma: "https://www.figma.com/proto/bTkxc3r2SOpZSpISxn3mQQ/TEENU2?page-id=0%3A1&node-id=79-1734&p=f&viewport=-654%2C359%2C0.6&t=TbsdZ2pHull7cFYt-1&scaling=scale-down&content-scaling=fixed",
@@ -25,11 +27,20 @@ const ProjectBox = ({ projectPhoto, projectName }) => {
         <br />
 
         {projectName === "CarPoint" ? (
-          <a href={desc[projectName + "Github"]} target="_blank" rel="noopener noreferrer">
-            <button className="projectbtn">
-              <FaGithub /> Github
-            </button>
-          </a>
+          <>
+            <a href={desc[projectName + "Website"]} target="_blank">
+              <button className="projectbtn">
+                <CgFileDocument /> Site
+              </button>
+            </a>
+
+            <a href={desc[projectName + "Github"]} target="_blank" rel="noopener noreferrer">
+              <button className="projectbtn">
+                <FaGithub /> Github
+              </button>
+            </a>
+            
+          </>
         ) : (
           <a href={desc[projectName + "Figma"]} target="_blank" rel="noopener noreferrer">
             <button className="projectbtn">
